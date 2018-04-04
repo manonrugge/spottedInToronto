438fead6eb06cf10a0992d6246e65e0149bf4086
Manon Rugge <manonrugge@gmail.com>
2018-04-04 19:05:36 -0400

    add content id content, images and text and two more categories

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -61,13 +61,14 @@ function initMap() {
 
     
     const icons = {
-        design: { icon: './public/assets/marker-pastel01.svg' },
-        maker: { icon: './public/assets/marker-pastel02.svg' },
-        bike: { icon: './public/assets/marker-pastel03.svg' },
-        brewery: { icon:'./public/assets/marker-pastel04.svg' },
-        vintageClothing: { icon:'./public/assets/marker-pastel05.svg' },
-        vintageHousewares: { icon: './public/assets/marker-pastel02' },
-        cafes: { icon: './public/assets/marker-pastel02' }
+        maker: { icon: './public/assets/marker-pastel01.svg' }, //maker green labels
+        design: { icon: './public/assets/marker-pastel02.svg' }, //design red/orange labels
+        bike: { icon: './public/assets/Marker.svg' }, //bike pink labels
+        brewery: { icon: './public/assets/marker-pastel04.svg' }, //brewery yellow labels
+        vintageClothing: { icon: './public/assets/marker-pastel05.svg' }, //vintageClothing grey/dark blue labels
+        vintageHousewares: { icon: './public/assets/marker-pastel05.svg' }, //vintageHousewares grey/blue labels
+        bar: { icon: './public/assets/marker-pastel07.svg' } //cafe brown labels
+        // x: //design purple labels
     };
 
 
@@ -76,13 +77,14 @@ function initMap() {
         [ '401 Richmond', 43.648138, -79.394525, 1 ],
         [ 'The Make Den', 43.659047, -79.440023, 2 ],
         [ 'The Shop', 43.652156, -79.433989, 3 ],
-        [ 'Art And Sole Academy', 43.660275, -79.329488, 4 ],
+        [ 'Art And Sole Academy', 43.660275, -79.329488, 4 ], //??
         [ 'The Workroom', 43.641671, -79.431887, 5 ],
         [ 'Artscape Youngplace', 43.646899, -79.417549, 6 ],
-        [ 'M.O.C.C.A.', 43.654615, -79.445207, 7 ],
-        [ 'Design Exchange', 43.647708, -79.380329, 8 ],
+        // [ 'M.O.C.C.A.', 43.654615, -79.445207, 7 ], // gallery category?
+        // [ 'Design Exchange', 43.647708, -79.380329, 8 ], // gallery category?
         [ 'Art Metropole', 43.649778, -79.431774, 9 ],
-        [ 'Harbourfront Center', 43.638749, -79.382722, 10 ],
+        // [ 'Harbourfront Center', 43.638749, -79.382722, 10 ],
+        [ 'Victoire Boutique', 43.647102, -79.419739, 11],
     ];
 
     // Create markers
@@ -102,7 +104,7 @@ function initMap() {
     const makers = [
         ['Eleven Thirty', 43.652528, -79.433419, 1 ],
         ['YNOT', 43.668808, -79.466622, 2 ],
-        ['T.D.M.', 43.639014, -79.443965, 3 ],
+        ['Toronto Designer Market', 43.639014, -79.443965, 3 ],
         ['Peace Collective', 43.647199, -79.419655, 4 ],
         ['Bookhou', 43.651967, -79.408319, 5 ],
         ['Yo Sox', 43.647409, -79.402331, 6 ],
@@ -135,8 +137,7 @@ function initMap() {
         ['Blood Brothers Brewing', 43.669924, - 79.465349, 6],
         ['Indie Alehouse', 43.665551, -79.465349, 7],
         ['Burdock', 43.659736, -79.437849, 8],
-        ['Junction Brewery', 43.673430, -79.478605, 9],
-        ['Left Field Brewery', 43.673809, -79.330514, 10],
+        ['Left Field Brewery', 43.673809, -79.330514, 9],
     ];
 
     for (let i = 0; i < breweries.length; i++) {
@@ -156,12 +157,11 @@ function initMap() {
         ['Chosen', 43.649666, -79.434950, 1],
         ['Penny Arcade Vintage', 43.649244, -79.421890, 2],
         ['You, Babe', 43.649695, -79.435273, 3],
-        ['House of Vintage', 43.641766, -79.430381, 4],
-        ['96 Tears Vintage', 43.639011, -79.445266, 5],
-        ['Kensington Market', 43.655051, -79.401802, 6],
-        ['In Vintage We Trust', 43.639900, -79.440695, 7],
-        ['Mama Loves You Vintage', 43.647829, -79.399868, 8],
-        ['Public Butter Vintage', 43.642003, -79.430348, 9]
+        ['Public Butter Vintage', 43.642003, -79.430348, 4],
+        ['96 Tears Vintage', 43.639011, -79.445266, 5], // not done yet
+        ['Kensington Market', 43.655051, -79.401802, 6], // not done yet
+        ['In Vintage We Trust', 43.639900, -79.440695, 7], // not done yet
+        ['Mama Loves You Vintage', 43.647829, -79.399868, 8], // not done yet
     ];
 
     for (let i = 0; i < vintageClothing.length; i++) {
@@ -179,14 +179,14 @@ function initMap() {
     // VINTAGE HOUSEWARES
     const vintageHousewares = [
         ['Mrs. Huizenga', 43.639907, -79.446908, 1 ],
-        ['1698 Queen Antiques', 43.639078, -79.444947, 2],
-        ['Smash Salvage', 43.665587, -79.465542, 3 ],
-        ['Bungalow', 43.655750, -79.402398, 4 ],
+        ['1698 Queen Antiques', 43.639078, -79.444947, 2],// not done yet
+        ['Smash Salvage', 43.665587, -79.465542, 3], // not done yet
+        ['Bungalow', 43.655750, -79.402398, 4], // not done yet
         ['Elephant In The Attic', 43.650012, -79.434956, 5 ],
-        ['Door Number Two', 43.665540, -79.462566, 6 ],
-        ['The Art of Demolition', 43.665984, -79.464847, 7 ],
+        ['Door Number Two', 43.665540, -79.462566, 6], // not done yet
+        ['The Art of Demolition', 43.665984, -79.464847, 7], // not done yet
         ['Williams Design', 43.665835, -79.464590, 8 ],
-        ['Studio 1484', 43.640558, -79.437411, 9 ],
+        ['Studio 1484', 43.640558, -79.437411, 9], // not done yet
     ];
 
     for (let i = 0; i < vintageHousewares.length; i++) {
@@ -211,8 +211,24 @@ function initMap() {
     const DonValley = { lat: 4, lng: -7 };
 
 
-    // WORK CAFES
-    
+    // WORK bars
+    const bars = [
+        ['Sweaty Bettys', 43.645205, -79.416725, 1],
+        ['Pharmacy', 43.638963, -79.430450, 2],// not done yet
+        ['Black Dice', 43.649938, -79.434428, 3], // not done yet
+        ['Communist Daughter', 43.649769, -79.421266, 4], // not done yet
+    ];
+
+    for (let i = 0; i < bars.length; i++) {
+        const bar = bars[i];
+        const marker = new google.maps.Marker({
+            position: { lat: bar[1], lng: bar[2] },
+            map: map,
+            icon: icons['bar'].icon,
+            title: bar[0],
+            label: { text: `${bar[3]}`, color: "white" }
+        });
+    };
 } 
 
 
@@ -259,3 +275,11 @@ for (let i = 0; i < btns.length; i++) {
         this.className += " active";
     });
 }
+
+
+
+$('.link-nav').on('click', function () {
+    $('html').animate({
+        scrollTop: $('#spots').offset().top
+    }, 1000);
+});
